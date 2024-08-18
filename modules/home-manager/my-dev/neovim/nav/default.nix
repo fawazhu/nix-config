c534765ea1b9fc04b2414075aa96dbbb2a032561
Fawaz Hussain <fawazsana@gmail.com>
2024-08-18 16:00:54 +0100

    fix: use vim.diagnostic instead of trouble
    Trouble is not needed in my setup. Workspace diagnostics no longer works
    for jumping so a combination of telescope and builtin functions are used
    instead.

--- modules/home-manager/my-dev/neovim/nav/default.nix
+++ modules/home-manager/my-dev/neovim/nav/default.nix
@@ -11,12 +11,11 @@
       nvim-web-devicons
       telescope-nvim
       telescope-ui-select-nvim
-      trouble-nvim
     ];
+    xdg.configFile."nvim/after/plugin/diagnostic.lua".source = ./diagnostic.lua;
     xdg.configFile."nvim/after/plugin/harpoon.lua".source = ./harpoon.lua;
     xdg.configFile."nvim/after/plugin/quickfix.lua".source = ./quickfix.lua;
     xdg.configFile."nvim/after/plugin/telescope.lua".source = ./telescope.lua;
     xdg.configFile."nvim/after/plugin/tree.lua".source = ./tree.lua;
-    xdg.configFile."nvim/after/plugin/trouble.lua".source = ./trouble.lua;
   };
 }
---
+++ modules/home-manager/my-dev/neovim/nav/diagnostic.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', ']e', vim.diagnostic.goto_next, {})
+vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, {})
