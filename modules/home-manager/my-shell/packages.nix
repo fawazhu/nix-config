5b71d556b60be13076f467babaeda78b5cf47229
Fawaz Hussain <fawazsana@gmail.com>
2024-08-21 20:45:23 +0100

    feat: add additional utils to my-shell
    Add useful utilities to my-shell

--- modules/home-manager/my-shell/packages.nix
+++ modules/home-manager/my-shell/packages.nix
@@ -5,6 +5,6 @@
   ...
 }: {
   config = lib.mkIf config.my-shell.enable {
-    home.packages = with pkgs; [ripgrep jq xq-xml yq-go eza fzf fd socat];
+    home.packages = with pkgs; [ripgrep jq xq-xml yq-go eza fzf fd socat openssl_3_3 rsync dig tcpdump ffmpeg_7-full imagemagick graphviz];
   };
 }
