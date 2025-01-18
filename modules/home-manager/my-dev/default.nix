cf9de25665739e5c1f30ae020bee4fd5ccb5e7d8
Fawaz Hussain <fawazsana@gmail.com>
2025-01-18 22:53:18 +0000

    Upgrade flake.lock

--- modules/home-manager/my-dev/default.nix
+++ modules/home-manager/my-dev/default.nix
@@ -4,23 +4,6 @@
   ...
 }: let
   granted = pkgs.granted.overrideAttrs {env.CGO_ENABLED = 0;};
-  awscli2 = pkgs.awscli2.overrideAttrs (oldAttrs: {
-    postPatch = ''
-      substituteInPlace pyproject.toml \
-        --replace-fail 'flit_core>=3.7.1,<3.9.1' 'flit_core>=3.7.1' \
-        --replace-fail 'awscrt>=0.19.18,<=0.22.0' 'awscrt>=0.22.0' \
-        --replace-fail 'cryptography>=40.0.0,<43.0.2' 'cryptography>=43.0.0' \
-        --replace-fail 'distro>=1.5.0,<1.9.0' 'distro>=1.5.0' \
-        --replace-fail 'docutils>=0.10,<0.20' 'docutils>=0.10' \
-        --replace-fail 'prompt-toolkit>=3.0.24,<3.0.39' 'prompt-toolkit>=3.0.24'
-      substituteInPlace requirements-base.txt \
-        --replace-fail "wheel==0.43.0" "wheel>=0.43.0"
-      # Upstream needs pip to build and install dependencies and validates this
-      # with a configure script, but we don't as we provide all of the packages
-      # through PYTHONPATH
-      sed -i '/pip>=/d' requirements/bootstrap.txt
-    '';
-  });
 in {
   options.my-dev = import ./options.nix {inherit lib;};
 
