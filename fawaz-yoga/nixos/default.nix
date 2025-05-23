ac663c7019ae54f71b871000595950fca182af35
Fawaz Hussain <fawazsana@gmail.com>
2025-05-23 18:24:40 +0100

    Revert "add howdy"
    This reverts commit d3ed2f0ba96a92650c1387ae9fce1588a5bf6a7b.

--- fawaz-yoga/nixos/default.nix
+++ fawaz-yoga/nixos/default.nix
@@ -1,7 +1,4 @@
-{pkgs, ...}: let
-  irPackage = pkgs.callPackage ./linux-enable-ir-emitter.nix {};
-  howdyPackage = pkgs.callPackage ./howdy.nix {};
-in {
+{pkgs, ...}: {
   imports = [
     ../../modules/nixos/my-desktop
     ../../modules/nixos/system
@@ -21,43 +18,10 @@ in {
     extraGroups = ["wheel"];
     shell = pkgs.zsh;
   };
-  nix.settings.trusted-users = ["fawaz"];
+  nix.settings.trusted-users = [ "fawaz" ];
 
   environment.localBinInPath = true;
   programs.zsh.enable = true;
   networking.hostName = "fawaz-yoga";
   system.stateVersion = "24.11";
-
-  environment.systemPackages = [irPackage howdyPackage];
-
-  systemd.services.linux-enable-ir-emitter = let
-    targets = [
-      "suspend.target"
-      "sleep.target"
-      "hybrid-sleep.target"
-      "hibernate.target"
-      "suspend-then-hibernate.target"
-    ];
-  in {
-    description = "Enable the infrared emitter";
-
-    script = "${irPackage}/bin/linux-enable-ir-emitter run";
-    serviceConfig.ConfigurationDirectory = "linux-enable-ir-emitter";
-    serviceConfig.StateDirectory = "linux-enable-ir-emitter";
-    wantedBy = targets ++ ["multi-user.target"];
-    after = targets ++ ["dev-video2.device"];
-  };
-  system.activationScripts.pam_howdy = ''
-    mkdir -p /etc/pam.d/
-    test -e /etc/pam.d/pam_howdy.so >/dev/null || ln -s "${howdyPackage}/lib/security/pam_howdy.so" /etc/pam.d/pam_howdy.so
-  '';
-  environment.etc."howdy/config.ini".text = ''
-    [video]
-    device_path = /dev/video2
-  '';
-
-  services.logind.extraConfig = ''
-    HandlePowerKey=ignore
-    HandleLidSwitch=suspend
-  '';
 }
