adce27a2998d3f3af89c915393e7bdad78444d13
Fawaz Hussain <fawazsana@gmail.com>
2025-09-04 17:10:49 +0100

    test removing gamescope fix

--- fawaz-yoga/nixos/default.nix
+++ fawaz-yoga/nixos/default.nix
@@ -21,17 +21,5 @@
 
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
-  programs.gamescope.package =
-    pkgs.gamescope.overrideAttrs
-    {
-      version = "master";
-      src = pkgs.fetchFromGitHub {
-        owner = "ValveSoftware";
-        repo = "gamescope";
-        fetchSubmodules = true;
-        rev = "1faf7acd90f960b8e6c816bfea15f699b70527f9";
-        hash = "sha256-/JMk1ZzcVDdgvTYC+HQL09CiFDmQYWcu6/uDNgYDfdM=";
-      };
-    };
   programs.gamescope.enable = true;
 }
